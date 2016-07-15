30a1653bbf7971e7399dee58a34e2d16d627fa4e
Amresh Kumar <amresh.kumar@ids-logic.local>
2016-07-15 14:08:34 +0530

    Update plugins and add animate

--- loadmore-onpage/loadmore.js
+++ loadmore-onpage/loadmore.js
@@ -27,7 +27,8 @@
             self.firstLoad = [];
             self.firstLoad.push(self.lists.splice(0, self.options.limit));
             //$(self.lists).not(self.firstLoad[0]).fadeOut();
-            $(self.firstLoad[0]).fadeIn();
+            //$(self.firstLoad[0]).fadeIn();
+            self.show(self.firstLoad[0]);
         },
         More: function() {
             var self = this;
@@ -35,17 +36,32 @@
             $(self.options.loadBtn).on('click', function() {
                 i += 1;
                 if (self.lists.length) {
-                    self.firstLoad.push(self.lists.splice(0, self.options.loadItem));
-                    $(self.firstLoad[i]).fadeIn();
+                    self.firstLoad.push(self.lists.splice(0, self.options.load));
+                    //$(self.firstLoad[i]).fadeIn();
+                    self.show(self.firstLoad[i]);
                 }
                 if (self.lists.length == 0) {
                     self.Out();
                 }
             });
         },
+        show: function(e) {
+            var self = this;
+            $(e).fadeIn();
+            if (self.options.animate) {
+                //$(e).addClass("animated " + self.options.animateIn);
+                self.animateCss(self.options.animateIn, e);
+            }
+        },
         Out: function() {
             var self = this;
             $(self.options.loadBtn).hide();
+        },
+        animateCss: function(animationName, elem) {
+            var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+            $(elem).addClass('animated ' + animationName).one(animationEnd, function() {
+                $(elem).removeClass('animated ' + animationName);
+            });
         }
     });
 
@@ -63,7 +79,9 @@
     $.fn[PluginName].default = {
         selector: '',
         limit: 3,
-        loadItem: 3,
-        loadBtn: ''
+        load: 3,
+        loadBtn: '',
+        animate: true,
+        animateIn: 'fadeInUp'
     };
 }(jQuery, window, document));
